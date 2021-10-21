e7f347d398df872c727c2db62745655bade3d633
FacuJardon <facundo.jardon@gmail.com>
2021-10-20 21:29:15 -0300

    correciones en clase cryptoScan

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import { cryptoScan } from "./models/models";
 
 let btcScanner = new cryptoScan(1, 10.0, 20.0);
-let ethScanner = new cryptoScan(2, 10.0, 20.0);
+let ethScanner = new cryptoScan("ETHUSDT", 10.0, 20.0);
 
-btcScanner.scanCrypto();
-ethScanner.scanCrypto();
+btcScanner.startScan();
+ethScanner.startScan();
--- src/models/cryptoScan.model.ts
+++ src/models/cryptoScan.model.ts
@@ -6,32 +6,77 @@ import { configModel, ICriptoPriceService } from './models';
 
 export default class cryptoScan {
     coinId:number | string;
-    nextTopAlarm?:number;
-    nextBottomAlarm?:number;
+    topAlarm:number | null = null;
+    bottomAlarm:number | null = null;
+    scanning:boolean = false;
     
-    constructor(coinId:number | string, nextTopAlarm?:number, nextBottomAlarm?:number)   {
+    constructor(coinId:number | string, topAlarm?:number, bottomAlarm?:number)   {
         
         if (typeof coinId != 'string')
             this.coinId = this.getCryptoPairById(coinId);
         else this.coinId = coinId;
 
-        if (nextBottomAlarm !== undefined)
-            this.nextBottomAlarm = nextBottomAlarm;
-        if (nextTopAlarm !== undefined)
-            this.nextTopAlarm = nextTopAlarm;
+        if (bottomAlarm !== undefined)
+            this.bottomAlarm = bottomAlarm;
+        if (topAlarm !== undefined)
+            this.topAlarm = topAlarm;
+    }
+
+    get isScanning():boolean {
+        return this.scanning;
     }
 
     getCryptoPairById(id:number):string {
         return configModel.getCoinSymbolByID(id);
     }
+    stopScan()  {
+        this.scanning = false;
+    }
+    startScan()    {
+        
+        if (this.isNullAlarms())  {
+            console.log("Alarmas no seteadas.");
+            return;
+        }
+        if (this.isScanning)    {
+            console.log("El scanner ya esta activo.");
+            return;
+        }
+        this.scanning = true;
+        this.requestApi();
+    }
 
-    async scanCrypto() {
-    
+    isNullAlarms():boolean  {
+        return this.topAlarm == null || this.bottomAlarm == null
+    }
+
+    async requestApi() {
+
         let url:string =  configModel.getValueByName("binanceApiUri")+this.coinId;
         let res:AxiosResponse<ICriptoPriceService> = await axios.get(url);
         
         if (res.data.price != undefined)
-            console.log(this.coinId+": "+res.data.price);
-        setTimeout(() => this.scanCrypto(), 1000);
+            this.validateAlarms(parseFloat(res.data.price));
+        else console.log("ApiError: Status: ["+res.status+"] "+res.data);
+
+        if (this.scanning)
+            setTimeout(() => this.requestApi(), 1000);
+    }
+
+    validateAlarms(price:number) {
+        if (this.topAlarm !== null && this.topAlarm! < price)   {
+            console.log(this.coinId+": Alarma superior excedida: "+price);
+            this.topAlarm = this.getNextTopAlarm();
+        }
+        if (this.bottomAlarm !== null && this.bottomAlarm! > price) {
+            console.log(this.coinId+": Alarma inferior excedida: "+price);
+            this.bottomAlarm = this.getNextBottomAlarm();
+        }
+    }
+    getNextTopAlarm():number {
+        return 3945.0;
+    }
+    getNextBottomAlarm():number {
+        return 3920.0;
     }
 }
